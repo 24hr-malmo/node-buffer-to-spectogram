087d5e6cc1d53f7daa42debc9ead0d79c184be53
Camilo Tapia <camilo.tapia@gmail.com>
2018-11-05 23:22:02 +0100

    first commit

---
+++ dev/single.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const bufferToSpectogram = require('../');
+
+const start = async () => {
+
+
+    const wavFile = path.join(__dirname, './data/12_11_01-sound-espresso.wav');
+    // const wavFile = path.join(__dirname, './data/sound-0001.wav');
+    const spectrogramFile = path.join(__dirname, './output/spectogram.png');
+    const buffer = fs.readFileSync(wavFile);
+
+    try {
+
+        let result = await bufferToSpectogram(buffer, spectrogramFile, {isWav: true});
+
+        result.forEach(item => {
+            console.log(item.filename);
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+
+};
+
+start();
